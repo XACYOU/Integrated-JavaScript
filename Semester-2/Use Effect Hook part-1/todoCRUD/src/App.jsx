37c370110cdb3792b566c6949b6e70726831647e
Saurabh Negi <xacyou20@gmail.com>
2023-12-26 15:59:54 +0530

    added todo crud

--- Semester-2/Use Effect Hook part-1/todoCRUD/src/App.jsx
+++ Semester-2/Use Effect Hook part-1/todoCRUD/src/App.jsx
@@ -1,117 +1,122 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import "./index.css";
+import { useState, useEffect } from "react"
+import axios from "axios"
 
-export default function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [task, setTask] = useState("");
 
-  async function fetchTodo() {
+function App() {
+
+  const [inpValue,setInpValue]=useState('');
+  const [taskList,setTaskList]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState(false);
+  const url='http://localhost:3000/todo/'
+
+  async function fetchTask(){
+    setLoading(true);
     try {
-      const res =
-        await axios.get(`https://jsonplaceholder.typicode.com/todos?_page=1&_limit=10
-      `);
-      setTodoList(res.data);
+      let res=await axios.get(url);
+      setTaskList(res.data);
       setLoading(false);
-    } catch (err) {
-      setError(err);
-      console.log(error)
+    } catch (error) {
+      setError(true);
       setLoading(false);
     }
   }
 
-  useEffect(() => {
-    fetchTodo();
-  }, []);
+  useEffect(()=>{
+    fetchTask()
+  },[])
+  
+  async function AddTask(){
 
-  async function addTask() {
-    if (task.trim() !== "") {
-      try {
-        const res = await axios.post(
-          `https://jsonplaceholder.typicode.com/todos`,
-          {
-            title: task,
-            completed: false,
-          }
-        );
+    try {
 
-        const updater = [...todoList, res.data];
-        setTodoList(updater);
-        setTask("");
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
+      const newTaskList={
+        id:Math.random() +inpValue,
+        title:inpValue,
+        completed:false
       }
+      // const updatedTaskList=[...taskList,newTaskList];
+      // setTaskList(updatedTaskList);
+      await axios.post(url,newTaskList)
+      setInpValue('');
+      fetchTask();
+      
+    } catch (error) {
+      console.log("Error Adding Task",error);
+    }
+  }
+
+  async function toggleStatus(id){
+
+    try {
+      
+      const updatedTaskList= taskList.map((task)=>{
+        if(task.id==id){
+         return {
+          ...task,
+          completed:!task.completed
+        };
+        }else{
+        return task
+        }
+      })
+      setTaskList(updatedTaskList);
+      await axios.patch(url+id,{completed:updatedTaskList.find((task)=> task.id==id).completed})
+
+    } catch (error) {
+      console.log("Error Toggling",error);
     }
   }
   
-  async function toggleStatus(id) {
-    try {
-      const updated = todoList.map((todo) =>
-        todo.id == id ? { ...todo, completed: !todo.completed } : todo
-      );
-      setTodoList(updated);
-
-      await axios.patch(`https://jsonplaceholder.typicode.com/todos/${id}`,{
-        completed:!todoList.find((todo) => todo.id == id).completed
-      })
-      setLoading(false);
-
-    } catch (err) {
-      console.log("Error Toggling", err);
-      setLoading(false);
-    }
+  async function deleteTask(id){
+    await axios.delete(url+id);
+    const updatedTaskList= taskList.filter((task)=> task.id!==id);
+    setTaskList(updatedTaskList);
   }
 
-  async function todoDelete(id) {
-    try {
-      const updated=todoList.filter((todo) => todo.id !== id)
-      setTodoList(updated)
-
-      await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      setLoading(false);
-
-    } catch (error) {
-      console.log(error)
-      setLoading(false);
-
-    }
+  function handleChange(e){
+    setInpValue(e.target.value);
   }
 
-  if (loading) {
-    return  <h1>Loading...</h1>;
+  if(loading) {
+    console.log('loading');
+  }
+
+  if(error){
+    console.log('error 404');
   }
 
   return (
     <>
-      <h1 style={{ textAlign: "center" }}> TODO - CRUD</h1>
-      <div className="inputBox">
-        <input
-          type="text"
-          value={task}
-          placeholder="Add Task"
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <button onClick={addTask}>ADD Task</button>
-      </div>
+    <h1 style={{textAlign:"center", fontSize:"50px", marginBottom:'50px'}}>Todo App</h1>
+    <div className="inputBox">
+      <input className="input" placeholder="Add Task" onChange={handleChange} value={inpValue} />
+      <button onClick={AddTask} id="btn" style={{fontSize:'30px'}}> ADD TODO</button>
+    </div>
 
-      <div className="todoBox">
-        {todoList.map((todo) => (
-          <div key={todo.id} className="todoItem">
-            <h3>{todo.title}</h3>
-            {todo.completed ? (
+    {loading &&<p style={{textAlign:"center", fontSize:'100px', color:"yellow"}}> Loading... </p>}
+
+    {error &&<p style={{textAlign:"center", fontSize:'100px', color:"red"}}>Error 404</p>}
+
+    <div className="todoBox">
+      {taskList.map((task)=>{
+        return (
+          <div key={task.id} className="todoItem">
+            <h3>{task.title}</h3>
+            {task.completed ? (
               <h4 style={{ color: "yellowgreen" }}>Completed</h4>
             ) : (
               <h4 style={{ color: "red" }}>Not Completed</h4>
             )}
-            <button onClick={() => toggleStatus(todo.id)}>TOGGLE</button>
-            <button onClick={()=> todoDelete(todo.id)}>DELETE</button>
+            <button onClick={()=> toggleStatus(task.id)}> TOGGLE </button>
+            <button onClick={()=> deleteTask(task.id) }> DELETE </button>
           </div>
-        ))}
-      </div>
+
+        )
+      })}
+    </div>
     </>
-  );
+  )
 }
+
+export default App
